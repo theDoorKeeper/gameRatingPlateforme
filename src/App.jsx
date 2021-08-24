ec39cb81bfd955f1544939a4ac92105d8a6db3c8
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-24 23:47:53 +0100

    added detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './Components/Header';
 import LoginPage from './Components/LoginPage';
+import Details from './Components/Details';
 import SignUpPage from './Components/SignupPage';
 import Theme from './Components/Theme';
 
@@ -13,6 +14,7 @@ function App() {
       <Header setloginOverlay={setloginOverlay}/>
       <LoginPage overlayState={loginOverlay} setOverlayState={setloginOverlay} setSingUpOverlay={setSingUpOverlay}/>
       <SignUpPage overlayState={singUpOverlay} setOverlayState={setSingUpOverlay} setloginOverlay={setloginOverlay}/>
+      <Details/>
       
     </Theme>
   );
