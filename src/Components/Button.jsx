640a414b4ddb137c504acc29003311a29ff5fdca
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-24 01:00:42 +0100

    added button component

---
+++ src/Components/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledButton = styled.button`
+    height : 3rem;
+    width : 6rem;
+    border : none ;
+    background : ${props => props.theme.colors.backgroundBlack};
+    color : ${props => props.theme.colors.white};
+    cursor : pointer;
+    &:hover{
+     border : 1px solid ${props => props.theme.colors.primaryGreen};
+     color : ${props => props.theme.colors.primaryGreen};
+    }
+
+`
+
+
+function Button(props) {
+    const {name, type, onClick} = props
+    return (
+        <StyledButton type={type} onClick={onClick}>
+            {name}
+        </StyledButton>
+    )
+}
+
+export default Button
