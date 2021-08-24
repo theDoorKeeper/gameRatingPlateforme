9481141fc3271c6a9a78b962ef48679e2519fb19
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-24 02:25:02 +0100

    added sign up page and passsed overlay state as rpops

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import Header from './Components/Header';
 import LoginPage from './Components/LoginPage';
+import SignUpPage from './Components/SignupPage';
 import Theme from './Components/Theme';
 
 function App() {
   const [loginOverlay, setloginOverlay] = useState(false);
-
+  const [singUpOverlay, setSingUpOverlay] = useState(false);
+  
   return (
     <Theme>
       <Header setloginOverlay={setloginOverlay}/>
-      <LoginPage overlayState={loginOverlay} setOverlayState={setloginOverlay}/>
+      <LoginPage overlayState={loginOverlay} setOverlayState={setloginOverlay} setSingUpOverlay={setSingUpOverlay}/>
+      <SignUpPage overlayState={singUpOverlay} setOverlayState={setSingUpOverlay} setloginOverlay={setloginOverlay}/>
+      
     </Theme>
   );
 }
