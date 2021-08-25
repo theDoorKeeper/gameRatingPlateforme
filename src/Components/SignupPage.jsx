bcccca654206c5d9593b518dcd72fa6a8904e8d0
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-26 00:49:14 +0100

    added repeat password

--- src/Components/SignupPage.jsx
+++ src/Components/SignupPage.jsx
@@ -8,7 +8,7 @@ const FormWrapper = styled.div`
     display : flex;
     flex-direction : column;
     align-items : center ;
-    height : 65vh;
+    height : 70vh;
     width : 50vh;
     background-color  : ${props => props.theme.colors.backgroundGray};
     color : ${props => props.theme.colors.white};
@@ -36,20 +36,23 @@ function SignUpPage(props) {
 
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
+    const [repeatPassword, setRepeatPassword] = useState(null);
     const [name, setName] = useState(null);
 
-    const handleClick = ()=>{
+    const handleClick = ()=>{   
         setOverlayState(false);
         setloginOverlay(true);
     }
+
 
     return (
         <Overlay visible={overlayState} setVisible={setOverlayState}>
          <FormWrapper>
              <h1>Sign up</h1>
+              <Input label={"Name"} setValue={setName} type={"text"}/>  
               <Input label={"E-mail"} setValue={setEmail} type={"email"}/>  
               <Input label={"Password"} setValue={setPassword} type={"password"}/>  
-              <Input label={"Name"} setValue={setName} type={"text"}/>  
+              <Input label={"Repeat Password"} setValue={setRepeatPassword} type={"password"}/> 
               <Button name={"Sign Up"} type={"submit"}/>
                  <Bar/>
                  Already have an account? <Login onClick={handleClick}> Login </Login> 
