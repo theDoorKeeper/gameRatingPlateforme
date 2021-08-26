c1174c256619489b3460fe098862ba277bf32f6c
thteDoorKeeper <chakibou1@hotmail.com>
2021-08-26 19:51:51 +0100

    wrapped components with authprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
 import Header from './Components/Header';
 import LoginPage from './Components/LoginPage';
 import Details from './Components/Details';
 import SignUpPage from './Components/SignupPage';
 import Theme from './Components/Theme';
+import { AuthProvider } from './Components/AuthProvider';
 
 function App() {
-  const [loginOverlay, setloginOverlay] = useState(false);
-  const [singUpOverlay, setSingUpOverlay] = useState(false);
+	const [loginOverlay, setloginOverlay] = useState(false);
+	const [singUpOverlay, setSingUpOverlay] = useState(false);
   
-  return (
-    <Theme>
-      <Header setloginOverlay={setloginOverlay}/>
-      <LoginPage overlayState={loginOverlay} setOverlayState={setloginOverlay} setSingUpOverlay={setSingUpOverlay}/>
-      <SignUpPage overlayState={singUpOverlay} setOverlayState={setSingUpOverlay} setloginOverlay={setloginOverlay}/>
-      <Details/>
-      
-    </Theme>
-  );
+	return (
+    
+		<Theme>
+			<AuthProvider> 
+				<Header setloginOverlay={setloginOverlay}/>
+				<LoginPage overlayState={loginOverlay} setOverlayState={setloginOverlay} setSingUpOverlay={setSingUpOverlay}/>
+				<SignUpPage overlayState={singUpOverlay} setOverlayState={setSingUpOverlay} setloginOverlay={setloginOverlay}/>
+				<Details/>
+			</AuthProvider>
+		</Theme>
+	);
 }
 
 export default App;
