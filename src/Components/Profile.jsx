27913cfd5742afb04d7a5421515d3ef8844a72d5
thteDoorKeeper <chakibou1@hotmail.com>
2021-09-06 19:03:24 +0100

    remvoed mock  styled componnets and replaced them with React components

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth } from './AuthProvider';
+import ProfileDetails from './Profile/ProfileDetails';
+import ProfileImages from './Profile/ProfileImages';
 
 const IgnoreDiv = styled.div`
 	display : none ;
@@ -12,72 +14,6 @@ const Content = styled.div`
 	height : 100vh;
 `;
 
-const CoverPhoto = styled.div`
-	width : 100%;
-	height : 50%;
-	background-color : blue;
-`;
-
-const ProfileCard = styled.div`
-	position : relative;
-	width : 15rem;
-	height : 20rem;
-	display : flex;
-	flex-direction : column;
-	justify-content : center;
-	gap : 0.5rem;
-	left : 10%;
-	top : 50%;
-	z-index : 2;
-`;
-
-const ProfilePicture = styled.div`
-	width : 100%;
-	height : 15rem;
-	background-color : white;
-	border : 3px solid ${props => props.theme.colors.primaryGreen};
-`;
-
-const ProfileName = styled.div`
-	width : 100%;
-	height : 2rem;
-	font-size : ${props => props.theme.fontSizes.medium};
-	color : white;
-	text-align : center;
-`;
-
-const ProfileDate = styled.div`
-	width : 100%;
-	font-size : ${props => props.theme.fontSizes.small};
-	color : ${props => props.theme.colors.transparentWhite};
-	text-align : center;
-`;
-
-const DetailsContainer = styled.div`
-	  width : 100%;
-	  margin : auto;
-	  height: 50rem; ;
-	  background-color : red;	
-	  display : flex;
-	  flex-direction : column;
-	  align-items : center;
-`;
-
-const DetailsNavbar = styled.div`
-	width : 100%;
-	height : 3rem;
-	color : white;
-	font-size : ${props => props.theme.fontSizes.small};
-	background-color : black;
-`;
-
-const DetailsContent = styled.div`
-	width : 50%;
-	height : 30rem;
-	background-color : gray;
-
-`;
- 
 function Profile() {
 	const {logout,currentUser} = useAuth();
 	const history = useHistory();
@@ -94,17 +30,8 @@ function Profile() {
 				<button onClick={handleLogout}>Logout</button>
 			</IgnoreDiv>
 			<Content>
-				<CoverPhoto>
-					<ProfileCard>
-						<ProfilePicture/>
-						<ProfileName>Thedoorkeeper</ProfileName>
-						<ProfileDate>Joined on Dec 07, 2019</ProfileDate>
-					</ProfileCard>
-				</CoverPhoto>
-				<DetailsContainer>
-					<DetailsNavbar/>
-					<DetailsContent/>
-				</DetailsContainer>
+				<ProfileImages/>
+				<ProfileDetails/>
 			</Content>
 		</>
 	);
