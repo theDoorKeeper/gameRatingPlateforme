62c7180e766348c5e1beeac6ce284263ea7c9b15
thteDoorKeeper <chakibou1@hotmail.com>
2021-09-09 19:49:55 +0100

    added FOllowers component with mock display to test routing

---
+++ src/Components/Profile/Followers.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Followers() {
+	return (
+		<div>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+		</div>
+	);
+}
+
+export default Followers;
