d47ce10eb35f136f22e4b9f029c2526195d133ea
thteDoorKeeper <chakibou1@hotmail.com>
2021-09-09 19:49:41 +0100

    added lists component with mock display to test routing

---
+++ src/Components/Profile/Lists.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Lists() {
+	return (
+		<div>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			<p>TESTING DISPLAY TESTING DISPLAY</p>
+		</div>
+	);
+}
+
+export default Lists;
