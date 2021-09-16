493ded14b77ae21f237d3697459d99719256d48d
thteDoorKeeper <chakibou1@hotmail.com>
2021-09-16 21:18:49 +0100

    got rid of unecessary states and useEffect , only pass currentUser to child components

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -5,10 +5,6 @@ import styled from 'styled-components';
 import { useAuth } from './AuthProvider';
 import ProfileDetails from './Profile/ProfileDetails';
 import ProfileImages from './Profile/ProfileImages';
-import cover from '../assets/profileCoverTest.jpg';
-import profilePicture from '../assets/profilePicture.png';
-import { collection, doc, getDoc, getDocs, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../firebase';
 
 const IgnoreDiv = styled.div`
 	display : none ;
@@ -20,9 +16,6 @@ const Content = styled.div`
 `;
 
 function Profile() {
-
-	const [userData, setuserData] = useState({});
-	const [loading, setLoading] = useState(false);
 	const {logout,currentUser} = useAuth();
 	const history = useHistory();
 	let { path, url } = useRouteMatch();
@@ -32,50 +25,16 @@ function Profile() {
 		history.push('/');
 	};
 
-/* 	useEffect(() => {
-		const docRef = query(collection(db, 'users'), where("uid", "==", currentUser.uid));
-
-		const unsub = onSnapshot(docRef, (querySnapshot) => {
-			let data;
-			querySnapshot.forEach((doc) => {
-				data = doc.data();
-			});
-			setuserData(data)
-		  });
-
-		return unsub();
-
-	}, [currentUser]); */
-
-	
-	useEffect(() => {
-		const getData = async () => {
-			setLoading(true);
-			const docRef = query(collection(db, 'users'), where("uid", "==", currentUser.uid));
-
-			const docSnap = await getDocs(docRef);
-			docSnap.forEach((doc) => {
-				// doc.data() is never undefined for query doc snapshots
-				setuserData(doc.data());
-			  });
-			  
-			setLoading(false);
-		};
-
-		getData();
-
-	}, [currentUser]);
-
 	return (
 		<>
 			<>
-				{ !loading && 'this is his is '  + userData.eMail + ' profile' }
+				{ ' profile' }
 				<button onClick={handleLogout}>Logout</button>
 			</>
-			{ !loading && <Content>
-				<ProfileImages  user={userData} ready={!loading}/>
-				<ProfileDetails path={path} url={url} user={userData} ready={!loading} />
-			</Content>}
+			  <Content>
+				<ProfileImages  user={currentUser} ready={currentUser}/>
+				<ProfileDetails path={path} url={url} user={currentUser}/>
+			</Content>
 		</>
 	);
 }
