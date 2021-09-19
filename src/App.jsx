68a2e8790d9a105d8f4ada45d0f6fa2f7da691d4
thteDoorKeeper <chakibou1@hotmail.com>
2021-09-19 02:13:05 +0100

    added dynamic route for User component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PrivateRoute from './Components/PrivateRoute';
 import LoginPage from './Components/LoginPage';
 import SignUpPage from './Components/SignupPage';
 import Game from './Components/Game';
+import User from './Components/User';
 
 function App() {
 	const [loginOverlay, setloginOverlay] = useState(false);
@@ -39,6 +40,9 @@ function App() {
 						<PrivateRoute  path="/Profile" component={Profile}/>
 						<Route path="/Game/:name">
 							<Game/>
+						</Route>
+						<Route path="/Users/:name">
+							<User/>
 						</Route>
 
 					</Switch>
