20b56014c6f4c243d583cb1898e053d1d2f5b6e0
thteDoorKeeper <chakibou1@hotmail.com>
2021-10-05 04:12:46 +0100

    added wishedWrapper styled component

--- src/Components/Profile/Lists.jsx
+++ src/Components/Profile/Lists.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const WishedWrapper = styled.div`
+	width : 100%;
+	gap : 5px;
+	display : flex;
+	flex-wrap : wrap;
+	justify-content : center; 
+`;
 
 function Lists() {
 	return (
 		<div>
-			<p>TESTING DISPLAY TESTING DISPLAY</p>
-			<p>TESTING DISPLAY TESTING DISPLAY</p>
-			<p>TESTING DISPLAY TESTING DISPLAY</p>
-			<p>TESTING DISPLAY TESTING DISPLAY</p>
-			<p>TESTING DISPLAY TESTING DISPLAY</p>
+			Wished games :
+			<WishedWrapper>
+			</WishedWrapper> 
 		</div>
 	);
 }
