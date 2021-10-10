bfcc6019062ad1994703f88366e2b44ead2788af
thteDoorKeeper <chakibou1@hotmail.com>
2021-10-10 01:34:52 +0100

    passed name to profileDetails

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -65,7 +65,7 @@ function User() {
 		<>
 			{userData && <Content>
 				<ProfileImages  user={userData} ready={!loading} notUser={notUser}/>
-				<ProfileDetails path={path} url={url} user={userData} ready={!loading}  notUser={notUser}/>
+				<ProfileDetails path={path} url={url} user={userData} ready={!loading}  notUser={notUser} name={name}/>
 			</Content>}
 		</>
 	);
