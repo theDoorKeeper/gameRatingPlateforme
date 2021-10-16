ea381ebe70e98f3af69d7dc31b14232ecc6f28f7
thteDoorKeeper <chakibou1@hotmail.com>
2021-10-16 01:57:14 +0100

    added gamehub component with genre state

---
+++ src/Components/GameHub.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width:100%;
+    display:flex;
+    padding : 15px;
+`;
+
+const GameListContainer = styled.div`
+    width : 70%;
+    display : flex;
+    flex-wrap : wrap;
+`;
+
+
+const TypesContainer = styled.div`
+    width : 30%;
+    display : flex;
+    flex-direction : column;
+    gap : 10px;
+`;
+
+const TypeButton = styled.button`
+    width : 80%;
+    height : 3rem;
+    border-radius : 30px;
+    border : none ;
+`;
+
+
+function GameHub() {
+	const [genre, setGenre] = useState('');
+	
+	return (
+		<Container>
+			<TypesContainer>
+				<TypeButton>action</TypeButton>
+				<TypeButton>fps</TypeButton>
+				<TypeButton>simulation</TypeButton>
+			</TypesContainer>
+			<GameListContainer/>
+		</Container>
+	);
+}
+
+export default GameHub;
