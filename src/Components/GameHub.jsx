55c2a89a1c7c8586e92ea1d4869ef641391f83a1
thteDoorKeeper <chakibou1@hotmail.com>
2021-10-18 01:17:52 +0100

    added useffect with queryGame function

--- src/Components/GameHub.jsx
+++ src/Components/GameHub.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
@@ -36,11 +37,45 @@ function GameHub() {
 	const [genre, setGenre] = useState('');
 	const [gameList, setGameList] = useState([]);
 
+
+	const getGenre = (e)=>{
+		setGenre(e.target.textContent);
+		console.log(e.target.textContent);
+	};
+
+	useEffect(() => {
+		const queryGame = async () => {
+			axios({	
+				method: 'GET',
+				url: `https://rawg-video-games-database.p.rapidapi.com/games?genres=${genre}&key=43d9190ae1bb47f9a9a2276650e8b411`,
+				headers: {
+					'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+					'x-rapidapi-key': 'f99d20b2e9msh2a92c3966263053p1f15a8jsn4d1703a3f7b2'
+				},
+			}) 
+				.then((response) => {
+					console.log(response.data);
+					const tempArray = [];
+	
+					response.data.results.forEach( result =>{
+						tempArray.push(result);
+					});
+					setGameList(tempArray); 
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
+
+		queryGame();
+	}, [genre]);
+    
+
 	return (
 		<Container>
 			<TypesContainer>
-				<TypeButton>action</TypeButton>
-				<TypeButton>fps</TypeButton>
+				<TypeButton onClick={getGenre}>action</TypeButton>
+				<TypeButton onClick={getGenre}>fps</TypeButton>
 				<TypeButton>simulation</TypeButton>
 			</TypesContainer>
 			<GameListContainer/>
