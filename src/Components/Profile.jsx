84dfd4b34525f66c064e3c005ff738fbd8cd10bc
thteDoorKeeper <chakibou1@hotmail.com>
2021-10-24 16:49:15 +0100

    removed useffect subscritions , received userData and props from app component

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
@@ -5,19 +6,16 @@ import styled from 'styled-components';
 import { useAuth } from './AuthProvider';
 import ProfileDetails from './Profile/ProfileDetails';
 import ProfileImages from './Profile/ProfileImages';
-import { collection, getDocs, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../firebase';
 
 const Content = styled.div`
 	width : 100%;
 	height : 100vh;
 `;
 
-function Profile() {
+function Profile(props) {
+	const {userData, loading} = props;
 
-	const [userData, setuserData] = useState({});
-	const [loading, setLoading] = useState(false);
-	const {logout,currentUser} = useAuth();
+	const {logout} = useAuth();
 	const history = useHistory();
 	let { path, url } = useRouteMatch();
 
@@ -26,43 +24,6 @@ function Profile() {
 		history.push('/');
 	};
 
-	useEffect(() => {
-	
-		const docRef = query(collection(db, 'users'), where('uid', '==', currentUser.uid));
-
-		const unsub = onSnapshot(docRef, (querySnapshot) => {
-			setLoading(true); 
-			let data = {};
-			querySnapshot.forEach((doc) => {
-				data = doc.data();
-				console.log('edited');
-			});
-			setuserData(data);
-			setLoading(false);
-		});
-
-		return unsub;
-
-	},[]);
-
-	
-	useEffect(() => {
-		const getData = async () => {
-			setLoading(true);
-			const docRef = query(collection(db, 'users'), where('uid', '==', currentUser.uid));
-
-			const docSnap = await getDocs(docRef);
-			docSnap.forEach((doc) => {
-				// doc.data() is never undefined for query doc snapshots
-				setuserData(doc.data());
-			});
- 
-			setLoading(false);
-		};
-
-		getData();
-
-	}, []);
 
 	return (
 		<>
